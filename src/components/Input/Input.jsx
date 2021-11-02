ee14fcfad2054effe1e33b34b6ea839a1605c9f0
Diresh Kumar <41542536+diresh18@users.noreply.github.com>
2021-11-02 10:57:36 +0530

    added validation

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,18 +3,26 @@ import styles from "./Input.module.css";
 
 export const Input = ({ setData }) => {
   const [inputText, setInputText] = useState("");
+  const [showValidation, setShowValidation] = useState(false);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (inputText.length > 2) {
+      setData((data) => [
+        { id: data.length + 1, name: inputText, favourite: false },
+        ...data,
+      ]);
+      setInputText("");
+      setShowValidation(false);
+    } else {
+      setShowValidation(true);
+    }
+  };
 
   return (
     <form
       className={`${styles.input_form}`}
-      onSubmit={(event) => {
-        event.preventDefault();
-        setData((data) => [
-          { id: data.length + 1, name: inputText, favourite: false },
-          ...data,
-        ]);
-        setInputText("");
-      }}
+      onSubmit={(event) => submitHandler(event)}
     >
       <input
         type="text"
@@ -26,6 +34,14 @@ export const Input = ({ setData }) => {
       <button type="submit" className={`${styles.btn}`}>
         Add Friend
       </button>
+      {showValidation && inputText.length === 0 && (
+        <p className={`${styles.error}`}>Please enter a name !!</p>
+      )}
+      {showValidation && inputText.length < 3 && inputText.length > 0 && (
+        <p className={`${styles.error}`}>
+          Name cannot be less than 3 characters!!
+        </p>
+      )}
     </form>
   );
 };
